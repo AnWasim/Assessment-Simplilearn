a20b2f31004acdbcf85091e021bb1bd249358d9c
AnWasim <ansariwasim7984@gmail.com>
2022-08-04 15:44:06 +0530

    free comit

---
+++ CallPubAcc.java
@@ -0,0 +1,15 @@
+package pack02;
+
+import pack04.PubAccSpec;
+
+public class CallPubAcc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PubAccSpec obj=new PubAccSpec();
+		obj.Ansari();
+
+	}
+
+}
